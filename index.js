fb232e336ca47004410832a414b89b5fc5f4a43c
Walter Melo <41174751+walmello24@users.noreply.github.com>
2022-03-08 10:21:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-	const $$ = query => Array.from(document.querySelectorAll(query))
+
+
+const $$ = query => Array.from(document.querySelectorAll(query))
   let dark_mode = false
   
   $$('[wt-dark_mode]').map(element => {
@@ -22,3 +24,65 @@
       })
     }
   }
+
+//////
+
+const Values = {
+    name:() => 'Walter',
+    val1:() => 0,
+    val2:() => 1,
+    result:() => Data.val1 + Data.val2
+}
+
+
+
+const WT_Initialize = (Values) => {    
+    window.WT_Data = new Proxy(Values,{
+        get: (obj, prop) => {
+            return obj[prop]()
+        },
+        set: (obj, prop, value) => {
+            obj[prop] = () => value
+            Update()
+            return obj[prop]
+        }
+    })
+
+    const App = (update = () => {}) => (action = () => {}) => {
+        action()
+        update()
+    }
+
+    const Update = App(() => {
+        const Each = (query, action) => Array
+            .from(document.querySelectorAll(query))
+            .map(item => action(item))
+
+        Each('[wt-bind]', item => {
+            let variable = item.getAttribute('wt-bind')
+            
+            
+
+            item.oninput = () => {                
+                Data[variable] = item.value
+            }
+            item.value = Data[variable]
+        })
+
+        Each('[wt-edit]', item => {
+            let variable = item.getAttribute('wt-edit')
+            
+            item.oninput = () => {
+                Data[variable] = item.innerText
+            }
+            item.innerText = Data[variable]
+        })
+
+        Each('[wt-text]', item => {
+            let variable = item.getAttribute('wt-text')            
+            item.innerText = Data[variable]
+        })
+    })		
+
+    Update()
+}
