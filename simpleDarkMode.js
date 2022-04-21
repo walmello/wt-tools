fd869b76dac7226a69ed0a8e1b0c0ca2f153ee47
Walter Melo <41174751+walmello24@users.noreply.github.com>
2022-04-21 10:52:11 -0300

    Create simpleDarkMode.js

---
+++ simpleDarkMode.js
@@ -0,0 +1,11 @@
+let dark_mode = false
+
+function wt_dark_toggle(){
+    const items = Array.from(document.querySelectorAll('body *, body'))    
+    console.log(items)
+    items.map(item => {
+        item.classList.toggle('wt_dark')
+    })
+}
+
+wt_dark_toggle()
